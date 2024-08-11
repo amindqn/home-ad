e669d505bcecba1eb392dc3785127a5ee55f0ae9
AminDqn <59019810+amindqn@users.noreply.github.com>
2024-08-11 22:11:31 +0330

    fix api bug

--- src/api/adApi.ts
+++ src/api/adApi.ts
@@ -1,29 +1,75 @@
 import api from "./axiosInstance";
 import { Ad } from "../types/adTypes";
+import useAuth from "../hooks/useAuth";
+import useErrorHandling from "../hooks/useErrorHandler";
+import useToast from "../hooks/useToast";
 
-export const fetchAds = async (
-    page: number = 1,
-    limit: number = 100
-): Promise<Ad[]> => {
-    const response = await api.get(`/ads?_page=${page}&_limit=${limit}`);
-    return response.data;
+const useAdApi = () => {
+    const { authData: user } = useAuth();
+    const withErrorHandling = useErrorHandling();
+    const {addToast}=useToast()
+    const fetchAds = async (
+        page: number = 1,
+        limit: number = 100
+    ): Promise<Ad[]> => {
+        const response = await api.get(`/ads?_page=${page}&_limit=${limit}`);
+        return response.data;
+    };
+
+    const fetchUserAds = async (
+        page: number = 1,
+        limit: number = 100
+    ): Promise<Ad[]> => {
+        if (!user) {
+            return [];
+        }
+
+        const response = await api.get(
+            `/ads?userId=${user.id}&_page=${page}&_limit=${limit}`
+        );
+        return response.data;
+    };
+
+    const fetchAdById = async (id: string | undefined): Promise<Ad> => {
+        const response = await api.get(`/ads/${id}`);
+        return response.data;
+    };
+
+    const createAd = async (ad: Ad): Promise<Ad> => {
+        if (!user) {
+            throw new Error("User is not authenticated");
+        }
+
+        const response = await api.post(`/ads`, { ...ad, userId: user.id });
+        addToast("You are successfully add a new ad", "success");
+        return response.data;
+    };
+
+    const updateAd = async (id: string | undefined, ad: Ad): Promise<Ad> => {
+        if (!user) {
+            throw new Error("User is not authenticated");
+        }
+        const response = await api.put(`/ads/${id}`, {
+            ...ad,
+            userId: user.id,
+        });
+        addToast("Your ad is successfully updated", "success");
+        return response.data;
+    };
+
+    const deleteAd = async (id: string | undefined): Promise<void> => {
+        await api.delete(`/ads/${id}`);
+        addToast("Your ad is successfully deleted", "info");
+    };
+
+    return {
+        fetchAds: withErrorHandling(fetchAds),
+        fetchUserAds: withErrorHandling(fetchUserAds),
+        fetchAdById: withErrorHandling(fetchAdById),
+        createAd: withErrorHandling(createAd),
+        updateAd: withErrorHandling(updateAd),
+        deleteAd: withErrorHandling(deleteAd),
+    };
 };
 
-export const fetchAdById = async (id: string): Promise<Ad> => {
-    const response = await api.get(`/ads/${id}`);
-    return response.data;
-};
-
-export const createAd = async (ad: Ad): Promise<Ad> => {
-    const response = await api.post(`/ads`, ad);
-    return response.data;
-};
-
-export const updateAd = async (id: string, ad: Ad): Promise<Ad> => {
-    const response = await api.put(`/ads/${id}`, ad);
-    return response.data;
-};
-
-export const deleteAd = async (id: string): Promise<void> => {
-    await api.delete(`/ads/${id}`);
-};
+export default useAdApi;
--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,13 +1,67 @@
+import { useCookies } from "react-cookie";
+import { useMutation, useQueryClient } from "react-query";
 import api from "./axiosInstance";
+import useAuth from "../hooks/useAuth";
+import useErrorHandling from "../hooks/useErrorHandler";
+import useToast from "../hooks/useToast";
 
-export const login = async (email: string, password: string) => {
-    const response = await api.post(`/login`, { email, password });
-    return response.data;
+const useAuthApi = () => {
+    const [cookies, setCookie, removeCookie] = useCookies(["token"]);
+    const queryClient = useQueryClient();
+    const { setAuthData } = useAuth();
+    const withErrorHandling = useErrorHandling();
+    const { addToast } = useToast();
+
+    const login = async (email: string, password: string) => {
+        const response = await api.post(`/login`, { email, password });
+        setCookie("token", response.data.accessToken, { path: "/" });
+        setAuthData(response.data.user);
+        addToast("You are successfully logged in", "success");
+        return response.data;
+    };
+
+    const signup = async (
+        email: string,
+        password: string,
+        username: string,
+        phone?: string
+    ) => {
+        const response = await api.post(`/register`, {
+            email,
+            password,
+            username,
+            phone,
+        });
+        addToast("You are successfully signed up", "success");
+        return response.data;
+    };
+
+    const updateUser = async (
+        id: string | undefined,
+        data: { username?: string; phone?: string; password?: string }
+    ) => {
+        if (id) {
+            const response = await api.patch(`/users/${id}`, data);
+            addToast("You are successfully update your profile", "success");
+            return response.data;
+        }
+        throw new Error("id is invalid");
+
+    };
+
+    const logout = async () => {
+        removeCookie("token", { path: "/" });
+        queryClient.clear();
+        setAuthData(null);
+        addToast("You are successfully logged out", "success");
+    };
+
+    return {
+        login: withErrorHandling(login),
+        signup: withErrorHandling(signup),
+        logout: withErrorHandling(logout),
+        updateUser: withErrorHandling(updateUser),
+    };
 };
 
-export const signup = async (email: string, password: string) => {
-    const response = await api.post(`/register`, { email, password });
-    return response.data;
-};
-
-export const logout = () => api.post("/logout");
+export default useAuthApi;
